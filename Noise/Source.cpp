d2892b3ba1a9d8e3db5d932dbc95f15ff4317e3c
EdwardBrodskiy <49645295+EdwardBrodskiy@users.noreply.github.com>
2020-05-15 20:38:15 +0100

    simple mose position draw

--- Noise/Source.cpp
+++ Noise/Source.cpp
@@ -6,17 +6,41 @@ int width = 1920;
 
 int main() {
 
-	sf::RenderWindow window(sf::VideoMode(width, height), "MandelBrotSet", sf::Style::Fullscreen);
+	sf::RenderWindow window(sf::VideoMode(width, height), "Noise", sf::Style::Fullscreen);
 
 	// Object used to put the image on screen
-	sf::Texture image;
+	sf::Texture texture;
 	sf::Sprite sprite;
-	image.create(width, height);
-	sprite.setTexture(image);
+	texture.create(width, height);
+	sprite.setTexture(texture);
 	sprite.setPosition(0, 0);
 
-	while (true) {
+	sf::Uint8* pixels = new sf::Uint8[width * height * 4];
+
+	while (window.isOpen()) {
+
+		sf::Event evnt;
+		while (window.pollEvent(evnt)) {
+			switch (evnt.type) {
+			case sf::Event::Closed:
+				window.close();
+			}
+
+		}
+
+		window.clear(sf::Color::Black);
+
+		sf::Vector2i mouse_pos = sf::Mouse::getPosition(window);
+
+		pixels[(mouse_pos.x + mouse_pos.y * width) * 4] = 255;
+		pixels[(mouse_pos.x + mouse_pos.y * width) * 4 + 3] = 255;
+
+		texture.update(pixels);
+
 		window.draw(sprite);
+
+
+
 		window.display();
 	}
 	
